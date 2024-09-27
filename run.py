eac73c2e4a39b0d91f0e7b29870b9278dbd346e5
Daniel Koller <daniel@dakoller.net>
2024-09-27 21:43:35 +0200

    oatuh changes added for tt & bc

--- run.py
+++ run.py
@@ -8,11 +8,7 @@ from dotenv import load_dotenv, dotenv_values
 from pprint import pprint
 from flask import Flask, redirect, request, session, url_for, jsonify
 load_dotenv()
-#import yfinance as yf
-
-# Step 1: Parse the RSS feed
-RSS_FEED_URL = 'https://www.rnz.de/feed/139-RL_Heidelberg_free.xml'
-
+import json
 
 # Step 2: Connect to the ESC/POS printer
 printer_ip = '192.168.2.134'
@@ -60,8 +56,162 @@ def print_news():
     except Exception as e:
         return jsonify({"status": "error", "message": "Failed to print."}), 500
 
+def get_basecamp_access_token_accountid():
+
+    access_token = None
+
+    with open('.bc_token','r') as f:
+        access_token = f.read()
+
+    if not access_token:
+        print('Access token not there.')
+        exit(1)
+
+    # get account id
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'User-Agent': 'ESCPrinter (daniel@dakoller.net)',
+    }
+    r = requests.get('https://launchpad.37signals.com/authorization.json',headers= headers)
+    resp = r.json()
+    # look for bc3 product
+    account_id = None
+    for item in resp['accounts']:
+        if item['product'] == 'bc3':
+            account_id = item['id']
+
+    return access_token, account_id
+
+def get_ticktick_accesstoken():
+    access_token = None
+
+    with open('.tt_token','r') as f:
+        access_token = f.read()
+
+    if not access_token:
+        print('Access token not there.')
+        exit(1)
+
+    ## get account id
+    #headers = {
+    #    'Authorization': f'Bearer {access_token}',
+    #}
+
+    #r = requests.get('https://ticktick.com/open/v1/project',headers= headers)
+    #resp = r.json()
+
+    #pprint(resp)
+
+    return access_token
+
+def get_basecamp_tasks():
+    access_token, account_id = get_basecamp_access_token_accountid()
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'User-Agent': 'ESCPrinter (daniel@dakoller.net)',
+    }
+
+    # get projects
+    projects = requests.get(f'https://3.basecampapi.com/{account_id}/projects.json', headers=headers).json()
+
+    for project in projects:
+        #pprint(project)
+        project_id = project['id']
+
+        # get todo set id
+        todoset_id = None
+        for item in project['dock']:
+            if item['name'] == 'todoset':
+                todoset_id = item['id']
+
+        todolists = requests.get(f'https://3.basecampapi.com/{account_id}/buckets/{project_id}/todosets/{todoset_id}/todolists.json', headers=headers).json()
+        for todolist in todolists:
+            pprint(todolist)
+            todolist_id = todolist['id']
+
+            # get tasks finally
+            tasks = requests.get(f'https://3.basecampapi.com/{account_id}/buckets/{project_id}/todolists/{todolist_id}/todos.json',headers=headers)
+            pprint(tasks)
+    
+
+def get_ticktick_tasks():
+    access_token = get_ticktick_accesstoken()
+
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+
+    r = requests.get(f"https://api.ticktick.com/api/v2/project/all/completedInAll", headers=headers)
+    resp = r.text
+
+    pprint(resp)
+
+def get_ticktick_api():
+    access_token = get_ticktick_accesstoken()
+
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+
+    r = requests.get('https://api.ticktick.com/open/v1/project/inbox122326785/task/66b776cfc1b6d19b4d30a79d/',headers=headers)
+
+    pprint(r.text)
+
+@app.route('/basecamp_callback')
+def basecamp_callback():
+    request_data = request.args
+    code = request_data['code']
+
+    data = {
+        'client_id':    os.getenv('BASECAMP_CLIENT_ID'),
+        'type':         'web_server',
+        'redirect_uri': os.getenv('BASECAMP_CALLBACK_URL'),
+        'client_secret':os.getenv('BASECAMP_CLIENT_SECRET'),
+        'code':         code,
+    }
+    #pprint(data)
+
+    r = requests.post('https://launchpad.37signals.com/authorization/token', data=data)
+    response = r.json()
+    if 'access_token' in response.keys():
+        with open('.bc_token','w') as f:
+            f.write(response['access_token'])
+
+    return jsonify({"status": "success", "message": "Callback received!"}), 200
+
+@app.route('/ticktick_callback')
+def ticktick_callback():
+    request_data = request.args
+    code = request_data['code']
+
+    #pprint(code)
+
+    data = {
+        'client_id':    os.getenv('TICKTICK_CLIENT_ID'),
+        'grant_type':         'authorization_code',
+        'redirect_uri': os.getenv('TICKTICK_REDIRECT_URI'),
+        'client_secret':os.getenv('TICKTICK_CLIENT_SECRET'),
+        'code':         code,
+        'scope':        'tasks:read',
+    }
+    #pprint(data)
+
+    r = requests.post('https://ticktick.com/oauth/token', data=data)
+    response = r.json()
+    pprint(response)
+    if 'access_token' in response.keys():
+        with open('.tt_token','w') as f:
+            f.write(response['access_token'])
+
+    return jsonify({"status": "success", "message": "Callback received!"}), 200
+
 # Execute the print job
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    #pprint(f"Basecamp oAuth Link:  https://launchpad.37signals.com/authorization/new?type=web_server&client_id={ os.getenv('BASECAMP_CLIENT_ID') }&redirect_uri={ os.getenv( 'BASECAMP_CALLBACK_URL' )}")
+    pprint(f"Ticktick: https://ticktick.com/oauth/authorize?client_id={ os.getenv('TICKTICK_CLIENT_ID')}&scope=tasks:read&redirect_uri={os.getenv('TICKTICK_REDIRECT_URI')}&response_type=code")
+    #app.run(debug=True, host='0.0.0.0', port=5001)
     #print_news()
+    #get_basecamp_tasks()
+    #get_ticktick_tasks()
+    get_ticktick_api()
 
